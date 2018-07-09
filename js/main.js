cb8fc41dde3b9086f69cb310f66f4f35cc73d645
Devan Grose <devangrose@gmail.com>
2018-07-09 12:31:51 -0700

    added objects for cards

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'dimonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'dimonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -15,8 +37,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped: " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped: " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
